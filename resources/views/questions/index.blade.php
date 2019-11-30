560c735ff65fe40af36296a8b9ca1892cee13f04
Anas <anaspltx@gmail.com>
2019-11-30 13:01:32 +0800

    implemeted authrization using policies

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -40,12 +40,17 @@
                                         </h3>
 
                                         <div class="ml-auto">
-                                            <a href="{{ route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        <form class="form-delete" action="{{ route('questions.destroy', $question->id)}}" method="post">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                        </form>
+                                            @can('update', $question)
+                                                <a href="{{ route('questions.edit', $question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                            @endcan
+
+                                            @if (Auth::user()->can('delete', $question))
+                                                <form class="form-delete" action="{{ route('questions.destroy', $question->id)}}" method="post">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                                 </form>
+                                            @endif
                                         </div>
                                 </div>
                                 
